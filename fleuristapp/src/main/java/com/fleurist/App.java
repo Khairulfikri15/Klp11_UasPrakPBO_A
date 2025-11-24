a0d2e2654dde0738adb838c13b0753ca37c43233
Syarifah Najwa. <syarifahnajwa74@gmail.com>
2025-11-24 22:03:06 +0700

    Update App.java

--- fleuristapp/src/main/java/com/fleurist/App.java
+++ fleuristapp/src/main/java/com/fleurist/App.java
@@ -1,1 +1,31 @@
+package com.fleurist;
 
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class App extends Application {
+
+    private static Scene scene;
+
+    @Override
+    public void start(Stage stage) throws IOException {
+        // Memuat file FXML dari folder resources
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("/com/fleurist/view/login.fxml"));
+        
+        Parent root = fxmlLoader.load();
+        scene = new Scene(root, 450, 550);
+        
+        stage.setTitle("Fleurist - Toko Bunga");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
